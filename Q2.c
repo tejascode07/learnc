37d19c3e549a8a6384925f550f5802db7be037bb
tejascode07 <163705126+tejascode07@users.noreply.github.com>
2024-06-10 17:41:25 +0530

    guvi Que 2
    You are given with a number "N", find its cube.

---
+++ Q2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    //A Simple Hello World
+int num,cube;
+scanf("%d",&num);
+
+cube=num*num*num;
+    
+    //Getting input via STDIN
+ 
+    printf("%d", cube);
+    return 0;
+}
